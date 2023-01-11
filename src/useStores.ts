f80c34624e4ead54be287b99e1196df4756e45e3
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2023-01-12 00:24:52 +0100

    Removed tests and cleaned up code a bit

---
+++ src/useStores.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { ReactiveControllerHost } from "lit";
+import { WritableAtom } from "nanostores";
+import { StoreController } from ".";
+
+/**
+ * @decorator `@useStores`
+ * A decorator that creates a new `StoreController` for each atom
+ * and adds it to the host element.
+ * @param atoms The atoms to subscribe to.
+ * @returns A class decorator that adds a `StoreController` for each atom.
+ * @example
+ * ```ts
+ * import { atom } from 'nanostores';
+ * import { useStores } from 'nanostores/lit';
+ * import { LitElement, html } from 'lit';
+ * import { customElement } from 'lit/decorators.js';
+ *
+ * const count = atom(0);
+ *
+ * @customElement('my-element')
+ * @useStores(count)
+ * class MyElement extends LitElement {
+ *  render() {
+ *   return html\`Count: \${count.get()}\`;
+ *   }
+ * }
+ * ```
+ */
+export function useStores(...atoms: WritableAtom<unknown>[]) {
+  return <T extends new (...args: any[]) => ReactiveControllerHost>(
+    constructor: T
+  ) => {
+    return class extends constructor {
+      constructor(...args: any[]) {
+        super(...args);
+        // Create a new StoreController for each atom
+        for (const atom of atoms) {
+          new StoreController(this, atom);
+        }
+      }
+    };
+  };
+}
